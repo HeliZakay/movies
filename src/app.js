e965e43feaee500ad0f0e30147cb2c8e12046565
Heli <helizakay1@gmail.com>
2020-08-26 12:46:43 +0300

    Remove dummy movies data

--- src/app.js
+++ src/app.js
@@ -10,11 +10,6 @@ import {setTextFilter, setPersonFilter, sortByScore} from "./actions/filters";
 import getVisibleMovies from "./selectors/movies";
 
 const store = configureStore();
-store.dispatch(addMovie({ createdAt: -100, movieName: "Tarazan", score: 10, personName:"Heli", content: "Great Disney movie. It's pretty good you should probably watch"}));
-store.dispatch(addMovie({ createdAt: 20, movieName: "Mulan", score:9, personName:"Heli", content: "I really loved this movie when I was little, it is quite magical"}));
-store.dispatch(addMovie({ movieName: "Paycheck", score:9, content: "I personally really love this movie. It is very interesting"}));
-store.dispatch(addMovie({ createdAt:-200, movieName: "The boy next door", personName:"Oz", score:6, content: "Even though this movie is listed as \"Sexy\" there are not many \"Sexy\" scenes. This movie is more scary than sexy"}));
-console.log(store.getState());
 
 const jsx = (
     <Provider store={store}>
