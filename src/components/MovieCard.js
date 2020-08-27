5fcb9e30f5059cbc346b517fcfeb14f7e9685ef4
Heli <helizakay1@gmail.com>
2020-08-27 10:11:19 +0300

    Setup formatting for created at

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+import moment from "moment";
 
 const MovieCard = ({id, movieName, personName, score, content, createdAt}) => (
         <div>
             <Link to={`/edit/${id}`}><h3>Movie: {movieName}</h3></Link>
             <p>Recommender: {personName}</p>
             <p>Score: {score}</p>
-            <p>Content: {content}</p>         
+            <p>Content: {content}</p>      
+            <p>Created At: {moment(createdAt).format("MMMM D, YYYY")}</p>    
             
         </div>
 );
