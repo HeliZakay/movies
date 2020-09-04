2c0762109dca785b0205a37c2f84b8483a030817
Heli <helizakay1@gmail.com>
2020-09-04 23:36:57 +0300

    showing recommender name

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -19,7 +19,7 @@ export class MovieCard extends React.Component {
             ): (
                 <h3>Movie: {this.props.movieName}</h3>
             )}
-            {/* <p>Recommender: {this.props.personName}</p> */}
+            <p>Recommender: {this.props.personName}</p>
             <p>Score: {this.props.score}</p>
             <p>Content: "{this.props.content}"</p>      
             <p>Created At: {moment(this.props.createdAt).format("MMMM D, YYYY")}</p> 
