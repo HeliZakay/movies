556b95fe3f5b73e5deebc225963f4e3be540d658
Heli <helizakay1@gmail.com>
2020-09-05 20:03:07 +0300

    adding style to navlink

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import {connect} from "react-redux";
 import {startLogout} from "../actions/auth";
 
 export const Header = ({startLogout, username}) => (
-  <header>
-    <h1>Movies</h1>
-    {username && <h3><em>Hi {username.charAt(0).toUpperCase() + username.slice(1)}</em></h3>}
-    <NavLink to="/homePage" activeClassName="is-active">Home</NavLink>
+  <header className="header">
+    <div className="content-container">
+    <div className="header__content">
+    {username && <h3 className="header__greeting"><em>Hi {username.charAt(0).toUpperCase() + username.slice(1)}</em></h3>}
+    <Link className="header__title" to="/homePage">
+    <h1>Movies with Friends</h1>
+    </Link>
     <br/>
-    <NavLink to="/create" activeClassName="is-active">   Create Recommendation</NavLink>
-    <br/>
-    <NavLink to="/watchList" activeClassName="is-active">   My Watching list</NavLink>
-    <br/>
-    <NavLink to="/friends" activeClassName="is-active">   My Friends</NavLink>
-    <br/>
+    {/* <NavLink to="/create" activeClassName="is-active">   Create Recommendation</NavLink>
+    <br/> */}
+    <NavLink className="header__title" to="/watchList" activeClassName="is-active"> <h2>Watching list</h2></NavLink>
+    <NavLink className="header__title" to="/friends" activeClassName="is-active"><h2>Friends</h2> </NavLink>
     <button onClick={startLogout}>Logout</button>
+    </div>
+    </div>
   </header>
 );
 const mapDispatchToProps = (dispatch) => ({
