6c66aa0dc4864115f5cfdaa644c51e33e361fddf
Heli <helizakay1@gmail.com>
2020-09-07 10:29:25 +0300

    fixing flex of header and actions

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,16 +7,16 @@ export const Header = ({startLogout, username}) => (
   <header className="header">
     <div className="content-container">
     <div className="header__content">
-    {username && <h3 className="header__greeting"><em>Hi {username.charAt(0).toUpperCase() + username.slice(1)}</em></h3>}
+    {username && <h2 className="header__title"><em>Hi {username.charAt(0).toUpperCase() + username.slice(1)}</em></h2>}
     <Link className="header__title" to="/homePage">
     <h1>Movies with Friends</h1>
     </Link>
-    <br/>
+   
     {/* <NavLink to="/create" activeClassName="is-active">   Create Recommendation</NavLink>
     <br/> */}
     <NavLink className="header__title" to="/watchList" activeClassName="is-active"> <h2>Watching list</h2></NavLink>
     <NavLink className="header__title" to="/friends" activeClassName="is-active"><h2>Friends</h2> </NavLink>
-    <button className="button button--link" onClick={startLogout}>Logout</button>
+    <button className="header__title button button--link" onClick={startLogout}>Logout</button>
     </div>
     </div>
   </header>
